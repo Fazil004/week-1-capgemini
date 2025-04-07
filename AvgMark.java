0e9de32055731fb8e30067d975c2954c367e237d
aa6773 <aa6773@srmist.edu.in>
2025-04-07 12:30:31 +0530

    Added AvgMark program

---
+++ AvgMark.java
@@ -0,0 +1,9 @@
+class AvgMark {
+    public static void main(String[] args){
+       int math = 94;
+       int phy  = 95;
+       int chem = 96;
+       double avg = (math+phy+chem)/3;
+       System.out.println ("Sam average mark in PCM is: "+ avg);
+    }
+}
