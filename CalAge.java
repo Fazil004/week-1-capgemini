d06370015f133d85f852e3e5fb08d50bf61ca2ee
aa6773 <aa6773@srmist.edu.in>
2025-04-07 12:28:52 +0530

    Added CalAge program

---
+++ CalAge.java
@@ -0,0 +1,8 @@
+class  CalAge {
+    public static void main (String[] args){
+         int birthYear = 2000;
+         int curYear = 2024;
+         int age = curYear - birthYear;
+         System.out.println("Harry's age in 2024 is" + age);
+    }
+}
